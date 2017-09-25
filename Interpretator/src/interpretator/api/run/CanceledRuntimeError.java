252dabed2dfa3b31e5af45061f92d5b07b36a980
Alexander Simon <alexvsimon@hotmail.com>
2017-09-25 17:30:04 +0300

    remove empty license

--- Interpretator/src/interpretator/api/run/CanceledRuntimeError.java
+++ Interpretator/src/interpretator/api/run/CanceledRuntimeError.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package interpretator.api.run;
 
 import interpretator.api.ast.AST;
--- Interpretator/src/interpretator/parser/ParserError.java
+++ Interpretator/src/interpretator/parser/ParserError.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package interpretator.parser;
 
 /**
  *
- * @author as204739
+ * @author alex
  */
 public class ParserError {
     private final String message;
